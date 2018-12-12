c4b8515959d8222cb1d10a1d2d96a7c4a80885ff
tan <haitanle88@gmail.com>
2018-12-12 15:21:37 -0500

    portfolio project 1

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ menu.addEventListener('click', function(e){
 	drawer.classList.toggle('open');
 	e.stopPropagation();
 });
+
+var portfolio = document.querySelector('.project1');
+var portfolio_desc = document.querySelector('#portfolio_desc');
+
+
+portfolio.addEventListener('click', function(e){
+	portfolio_desc.classList.toggle('open');
+	e.stopPropagation();
+})
