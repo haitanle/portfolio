d492e4a57e6bc740eb0ff5a68c04853bb51e7df7
tan <haitanle88@gmail.com>
2018-12-17 10:23:48 -0500

    syntax update

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ var portfolio = document.querySelector('.project1');
 var portfolio_desc = document.querySelector('#portfolio');
 var description = document.querySelector('.description');
 
+
+
 portfolio.addEventListener('click', function(e){
 	movie_desc.classList.remove('open');
 	weather_desc.classList.remove('open');
@@ -22,8 +24,10 @@ portfolio.addEventListener('click', function(e){
 	e.stopPropagation();
 });
 
+
 var movie = document.querySelector('.project2');
 var movie_desc = document.querySelector('#movie');
+
 
 movie.addEventListener('click', function(e){
 	portfolio_desc.classList.remove('open');
@@ -32,8 +36,11 @@ movie.addEventListener('click', function(e){
 	e.stopPropagation();
 });
 
+
+
 var weather = document.querySelector('.project3');
 var weather_desc = document.querySelector('#weather');
+
 
 weather.addEventListener('click', function(e){
 	portfolio_desc.classList.remove('open');
