cf6b3aa23e13cff51b44d63e0b587655a2c84676
tan <haitanle88@gmail.com>
2018-12-21 10:15:08 -0500

    add logo and title name

--- js/app.js
+++ js/app.js
@@ -1,43 +1,53 @@
-var menu = document.querySelector('.menu_icon');
-var drawer = document.querySelector('.drawer');
-var body = document.querySelector('body');
+// var menu = document.querySelector('.menu_icon');
+// var drawer = document.querySelector('.drawer');
+// var body = document.querySelector('body');
 
-menu.addEventListener('click', function(e){
-	drawer.classList.toggle('open');
-	e.stopPropagation();
-});
+// menu.addEventListener('click', function(e){
+// 	drawer.classList.toggle('open');
+// 	e.stopPropagation();
+// });
 
-body.addEventListener('click', function(e){
-	drawer.classList.remove('open');
-});
+// body.addEventListener('click', function(e){
+// 	drawer.classList.remove('open');
+// });
 
-var portfolio = document.querySelector('.project1');
-var portfolio_desc = document.querySelector('#portfolio');
-var description = document.querySelector('.description');
+// var portfolio = document.querySelector('.project1');
+// var portfolio_desc = document.querySelector('#portfolio');
+// var description = document.querySelector('.description');
 
-portfolio.addEventListener('click', function(e){
-	movie_desc.classList.remove('open');
-	weather_desc.classList.remove('open');
-	portfolio_desc.classList.add('open');
-	e.stopPropagation();
-});
+// portfolio.addEventListener('click', function(e){
+// 	movie_desc.classList.remove('open');
+// 	weather_desc.classList.remove('open');
+// 	portfolio_desc.classList.add('open');
+// 	e.stopPropagation();
+// });
 
-var movie = document.querySelector('.project2');
-var movie_desc = document.querySelector('#movie');
+// var movie = document.querySelector('.project2');
+// var movie_desc = document.querySelector('#movie');
 
-movie.addEventListener('click', function(e){
-	portfolio_desc.classList.remove('open');
-	weather_desc.classList.remove('open');
-	movie_desc.classList.add('open');
-	e.stopPropagation();
-});
+// movie.addEventListener('click', function(e){
+// 	portfolio_desc.classList.remove('open');
+// 	weather_desc.classList.remove('open');
+// 	movie_desc.classList.add('open');
+// 	e.stopPropagation();
+// });
 
-var weather = document.querySelector('.project3');
-var weather_desc = document.querySelector('#weather');
+// var weather = document.querySelector('.project3');
+// var weather_desc = document.querySelector('#weather');
 
-weather.addEventListener('click', function(e){
-	portfolio_desc.classList.remove('open');
-	movie_desc.classList.remove('open');
-	weather_desc.classList.add('open');
-	e.stopPropagation();
+// weather.addEventListener('click', function(e){
+// 	portfolio_desc.classList.remove('open');
+// 	movie_desc.classList.remove('open');
+// 	weather_desc.classList.add('open');
+// 	e.stopPropagation();
+// });
+
+
+
+
+var click_icon = document.querySelector('.click_icon');
+
+click_icon.addEventListener('click', function(e){
+	//document.querySelector('.menu').style.width = '0%';
+	document.querySelector('.menu').classList.toggle('menu_toggle');
 });
