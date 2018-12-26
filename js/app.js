54de86259c2c6604aa588211d6c289d1ecb75b0f
tan <haitanle88@gmail.com>
2018-12-26 16:42:26 -0500

    project hide and show

--- js/app.js
+++ js/app.js
@@ -50,11 +50,47 @@ var click_icon = document.querySelector('.click_icon');
 click_icon.addEventListener('click', function(e){
 	//document.querySelector('.menu').style.width = '0%';
 	document.querySelector('.menu').classList.toggle('menu_toggle');
+	e.stopPropagation();
+});
+
+var portfolio = document.querySelector('.portfolio_tn');
+
+// TODO: refactor add/remove toggle 
+portfolio.addEventListener('click', function(e){
+	document.querySelector('#weather').classList.add('project_hide');
+	document.querySelector('#game').classList.add('project_hide');
+	document.querySelector('#movie').classList.add('project_hide');
+	document.querySelector('#portfolio').classList.remove('project_hide');
+	e.stopPropagation();
+});
+
+var movie = document.querySelector('.movie_tn');
+
+movie.addEventListener('click', function(e){
+	document.querySelector('#portfolio').classList.add('project_hide');
+	document.querySelector('#weather').classList.add('project_hide');
+	document.querySelector('#game').classList.add('project_hide');
+	document.querySelector('#movie').classList.remove('project_hide');
+	e.stopPropagation();
+});
+
+var weather = document.querySelector('.weather_tn');
+
+weather.addEventListener('click', function(e){
+	document.querySelector('#portfolio').classList.add('project_hide');
+	document.querySelector('#movie').classList.add('project_hide');
+	document.querySelector('#game').classList.add('project_hide');
+	document.querySelector('#weather').classList.remove('project_hide');
+	e.stopPropagation();
 });
 
 
-var project = document.querySelector('.project');
+var game = document.querySelector('.game_tn');
 
-project.addEventListener('click', function(e){
-	document.querySelector('#portfolio').classList.toggle('portfolio_toggle');
-})
+game.addEventListener('click', function(e){
+	document.querySelector('#portfolio').classList.add('project_hide');
+	document.querySelector('#movie').classList.add('project_hide');
+	document.querySelector('#weather').classList.add('project_hide');
+	document.querySelector('#game').classList.remove('project_hide');
+	e.stopPropagation();
+});
