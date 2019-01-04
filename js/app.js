77409438b3e7a11f8c8219914140f4e169290b29
tan <haitanle88@gmail.com>
2019-01-04 11:40:31 -0500

    cleanup

--- js/app.js
+++ js/app.js
@@ -1,60 +1,12 @@
-// var menu = document.querySelector('.menu_icon');
-// var drawer = document.querySelector('.drawer');
-// var body = document.querySelector('body');
-
-// menu.addEventListener('click', function(e){
-// 	drawer.classList.toggle('open');
-// 	e.stopPropagation();
-// });
-
-// body.addEventListener('click', function(e){
-// 	drawer.classList.remove('open');
-// });
-
-// var portfolio = document.querySelector('.project1');
-// var portfolio_desc = document.querySelector('#portfolio');
-// var description = document.querySelector('.description');
-
-// portfolio.addEventListener('click', function(e){
-// 	movie_desc.classList.remove('open');
-// 	weather_desc.classList.remove('open');
-// 	portfolio_desc.classList.add('open');
-// 	e.stopPropagation();
-// });
-
-// var movie = document.querySelector('.project2');
-// var movie_desc = document.querySelector('#movie');
-
-// movie.addEventListener('click', function(e){
-// 	portfolio_desc.classList.remove('open');
-// 	weather_desc.classList.remove('open');
-// 	movie_desc.classList.add('open');
-// 	e.stopPropagation();
-// });
-
-// var weather = document.querySelector('.project3');
-// var weather_desc = document.querySelector('#weather');
-
-// weather.addEventListener('click', function(e){
-// 	portfolio_desc.classList.remove('open');
-// 	movie_desc.classList.remove('open');
-// 	weather_desc.classList.add('open');
-// 	e.stopPropagation();
-// });
-
-
-
 var click_icon = document.querySelector('.click_icon');
 
 click_icon.addEventListener('click', function(e){
-	//document.querySelector('.menu').style.width = '0%';
 	document.querySelector('.drawer').classList.toggle('menu_toggle');
 	e.stopPropagation();
 });
 
 var portfolio = document.querySelector('.portfolio_tn');
 
-// TODO: refactor add/remove toggle 
 portfolio.addEventListener('click', function(e){
 	document.querySelector('#weather').classList.add('project_hide');
 	document.querySelector('#game').classList.add('project_hide');
